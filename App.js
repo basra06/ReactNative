b01e7bd9f429c4dce8587a31ec0bca110c8ec8ff
basra06 <47266546+basra06@users.noreply.github.com>
2020-12-02 22:38:39 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+/* import { StatusBar } from 'expo-status-bar'; */
+import React from 'react';
+/* import { StyleSheet, Text, View } from 'react-native';
+import Superman from './components/Superman'; */
+import Horizontal from './components/Horizontal';
+
+
+export default function App() {
+  return (
+      <Horizontal/>
+  );
+}
+
+/* const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    flexDirection:'row', 
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+  },
+}); */
